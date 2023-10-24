b92c0c40e7fa0769caad816e4d5e764f717d4574
Ferna1990 <fcrulo901312@gmail.com>
2023-10-24 16:18:57 -0300

    agregado mas animaciones y campos a completar obligatorios en el formulario v 1.0.0.0

--- java/script.js
+++ java/script.js
@@ -1,17 +1,7 @@
 
-import Vue from "vue";
-import app from "../Paginas/pagina5.html";
-import Vuelidate from "vuelidate";
 
-Vue.use(Vuelidate);
-
-Vue.config.productionTip = false;
-new Vue({
-    render: h=> h(app),
-}).$mount('#app1')
-
-
-
+const {createApp}=Vue
+const miApp=createApp
 
 
 
